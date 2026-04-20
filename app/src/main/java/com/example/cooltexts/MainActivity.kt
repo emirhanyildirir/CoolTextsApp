0959f86ae2fb9c76ce209b1022f078d75a65647d
VİCTUS <denizbozkrt42@gmail.com>
2026-04-20 15:23:03 +0300

    ben kodda güncellemeler yaptım

--- app/src/main/java/com/example/cooltexts/MainActivity.kt
+++ app/src/main/java/com/example/cooltexts/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var a = 10
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +18,4 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
-    cfmctfklmtfmghmnbm
 }
