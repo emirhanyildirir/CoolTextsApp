95e58d9c379914d1860eb36db625d2bd0d981758
VİCTUS <denizbozkrt42@gmail.com>
2026-04-20 15:28:48 +0300

    ben kodda güncellemeler yaptım

--- app/src/main/java/com/example/cooltexts/MainActivity.kt
+++ app/src/main/java/com/example/cooltexts/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    var a = 10
+    var aA = 20
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
